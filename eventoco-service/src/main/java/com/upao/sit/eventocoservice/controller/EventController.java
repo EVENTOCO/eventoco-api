129ce702512b46fb65a0b7d254c3f2192a2af029
Renato <87155421+Renato5Lara@users.noreply.github.com>
2024-05-06 14:50:42 -0500

    Update EventController.java

--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/controller/EventController.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/controller/EventController.java
@@ -2,3 +2,8 @@ package com.upao.sit.eventocoservice.controller;
 
 public class EventController {
 }
+@DeleteMapping("/eventos/{eventoId}")
+public ResponseEntity<Void> eliminarEvento(@PathVariable Long eventoId) {
+    eventoService.eliminarEvento(eventoId);
+    return ResponseEntity.noContent().build();
+}
