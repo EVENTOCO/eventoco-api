dae6da648b114076943ba91ae8aede8280ae8e84
Renato <87155421+Renato5Lara@users.noreply.github.com>
2024-05-06 14:53:29 -0500

    Update EventService.java

--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/service/EventService.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/service/EventService.java
@@ -2,3 +2,9 @@ package com.upao.sit.eventocoservice.service;
 
 public class EventService {
 }
+public void eliminarEvento(Long eventoId) {
+    Evento evento = eventoRepository.findById(eventoId)
+                .orElseThrow(() -> new ResourceNotFoundException("Evento no encontrado"));
+
+    eventoRepository.delete(evento);
+}
