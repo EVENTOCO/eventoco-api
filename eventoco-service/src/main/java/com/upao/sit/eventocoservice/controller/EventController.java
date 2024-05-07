dc3d59eb5f61d069c671e2a5f4647ba6b4c0d256
kmedinar1@upao.edu.pe <Kathia Medina Rodriguez>
2024-05-07 18:43:02 -0500

    Modificar_detalles_del_evento

--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/controller/EventController.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/controller/EventController.java
@@ -1,4 +1,26 @@
 package com.upao.sit.eventocoservice.controller;
 
+
+import com.upao.sit.eventocoservice.model.dto.EventRequestDTO;
+import com.upao.sit.eventocoservice.model.dto.EventResponseDTO;
+import com.upao.sit.eventocoservice.service.EventService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/events")
 public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PutMapping("/event/{id}")
+    public ResponseEntity<EventResponseDTO> updateEvent(@PathVariable Long id, @RequestBody EventRequestDTO eventDTO) {
+        EventResponseDTO updateEvent = eventService.updateEvent(id, eventDTO);
+        return new ResponseEntity<>(updateEvent, HttpStatus.OK);
+    }
 }
--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/service/EventService.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/service/EventService.java
@@ -1,4 +1,33 @@
 package com.upao.sit.eventocoservice.service;
 
+import com.upao.sit.eventocoservice.exception.ResourceNotFoundException;
+import com.upao.sit.eventocoservice.mapper.EventMapper;
+import com.upao.sit.eventocoservice.model.dto.EventRequestDTO;
+import com.upao.sit.eventocoservice.model.dto.EventResponseDTO;
+import com.upao.sit.eventocoservice.model.entity.Event;
+import com.upao.sit.eventocoservice.repository.EventRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EventService {
+
+    private final EventRepository eventRepository;
+    private final EventMapper mapper;
+
+    public EventService(EventRepository eventRepository, EventMapper mapper) {
+        this.eventRepository = eventRepository;
+        this.mapper = mapper;
+    }
+
+    @Transactional
+    public EventResponseDTO updateEvent(Long id, EventRequestDTO eventRequestDTO) {
+        Event event = eventRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Error: " + "Evento(" +id+ ") no encontrado"));
+        event.setTitle(eventRequestDTO.getTitle());
+        event.setDate(eventRequestDTO.getDate());
+        event.setPlace(eventRequestDTO.getPlace());
+        event.setDescription(eventRequestDTO.getDescription());
+        event = eventRepository.save(event);
+        return mapper.convertToDTO(event);
+    }
 }
