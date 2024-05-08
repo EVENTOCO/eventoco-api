e3828a6604ea1ec4db1cc9578803b19b3b609737
kmedinar1@upao.edu.pe <Kathia Medina Rodriguez>
2024-05-08 15:51:49 -0500

    add @PathVariable on changeEventImportant from EventController

--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/controller/EventController.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/controller/EventController.java
@@ -5,6 +5,7 @@ import com.upao.sit.eventocoservice.model.dto.EventResponseDTO;
 import com.upao.sit.eventocoservice.service.EventService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class EventController {
     }
 
     @GetMapping("/important/{id}")
-    public ResponseEntity<EventResponseDTO> changeEventImportant(Long id) {
+    public ResponseEntity<EventResponseDTO> changeEventImportant(@PathVariable Long id) {
         eventService.changeEventImportant(id);
         return ResponseEntity.ok().build();
     }
