2247d84dd52c178fe100354a23f97155b0f73578
kmedinar1@upao.edu.pe <Kathia Medina Rodriguez>
2024-05-07 20:15:17 -0500

    Configuración inicial

--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/repository/EventRepository.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/repository/EventRepository.java
@@ -1,5 +1,10 @@
 package com.upao.sit.eventocoservice.repository;
 
-public interface EventRepository {
+import com.upao.sit.eventocoservice.model.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+public interface EventRepository extends JpaRepository<Event, Long>{
+    Optional<Event> findById(Long id);
 }
--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/service/EventService.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/service/EventService.java
@@ -1,4 +1,16 @@
 package com.upao.sit.eventocoservice.service;
 
+import com.upao.sit.eventocoservice.mapper.EventMapper;
+import com.upao.sit.eventocoservice.repository.EventRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EventService {
+    private final EventRepository eventRepository;
+    private final EventMapper mapper;
+
+    public EventService(EventRepository eventRepository, EventMapper mapper) {
+        this.eventRepository = eventRepository;
+        this.mapper = mapper;
+    }
 }
