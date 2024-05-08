05f3117788dd5d33b89d5411ee95a54a95e5ba65
kmedinar1@upao.edu.pe <Kathia Medina Rodriguez>
2024-05-08 15:27:08 -0500

    marcar_evento_como_importante

--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/controller/EventController.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/controller/EventController.java
@@ -1,4 +1,27 @@
 package com.upao.sit.eventocoservice.controller;
 
+
+import com.upao.sit.eventocoservice.model.dto.EventResponseDTO;
+import com.upao.sit.eventocoservice.service.EventService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
 public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/important/{id}")
+    public ResponseEntity<EventResponseDTO> changeEventImportant(Long id) {
+        eventService.changeEventImportant(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/service/EventService.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/service/EventService.java
@@ -1,8 +1,12 @@
 package com.upao.sit.eventocoservice.service;
 
+import com.upao.sit.eventocoservice.exception.ResourceNotFoundException;
 import com.upao.sit.eventocoservice.mapper.EventMapper;
+import com.upao.sit.eventocoservice.model.dto.EventResponseDTO;
+import com.upao.sit.eventocoservice.model.entity.Event;
 import com.upao.sit.eventocoservice.repository.EventRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EventService {
@@ -12,5 +16,14 @@ public class EventService {
     public EventService(EventRepository eventRepository, EventMapper mapper) {
         this.eventRepository = eventRepository;
         this.mapper = mapper;
+    }
+
+    @Transactional(readOnly = true)
+    public EventResponseDTO changeEventImportant(Long id) {
+        Event event = eventRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Evento "+id+" no encontrado"));
+        event.setImportant(true);
+        eventRepository.save(event);
+        return mapper.convertToDTO(event);
     }
 }
