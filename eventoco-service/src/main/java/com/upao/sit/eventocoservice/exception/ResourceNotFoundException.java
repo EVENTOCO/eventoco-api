159bed2a51dc9f0512bd3fa1126c66fa82535aaa
Renato <87155421+Renato5Lara@users.noreply.github.com>
2024-06-19 01:05:03 -0500

    Update ResourceNotFoundException.java

--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/exception/ResourceNotFoundException.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.upao.sit.eventocoservice.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
