d38a15ab2e5725b024d10c78a77c732117426864
Renato <87155421+Renato5Lara@users.noreply.github.com>
2024-06-19 01:03:43 -0500

    Update GlobalExceptionHandler.java

--- eventoco-service/src/main/java/com/upao/sit/eventocoservice/exception/ClobalExceptionHandler.java
+++ eventoco-service/src/main/java/com/upao/sit/eventocoservice/exception/ClobalExceptionHandler.java
@@ -1,1 +1,20 @@
+package com.upao.sit.eventocoservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidDataException.class)
+    public ResponseEntity<String> handleInvalidDataException(InvalidDataException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
